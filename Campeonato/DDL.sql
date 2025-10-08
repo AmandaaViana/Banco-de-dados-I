1c49b67d301c0500942b3c25eb6a7d77f8dffd17
AmandaaViana <amandaviana.ahv@gmail.com>
2025-10-08 15:30:21 -0300

    Update DDL.sql

--- Campeonato/DDL.sql
+++ Campeonato/DDL.sql
@@ -2,7 +2,7 @@ CREATE DATABASE campeonato;
 
 CREATE TABLE Campeonato 
 ( 
- id_Campeonato INT PRIMARY KEY,  
+ id_Campeonato SERIAL PRIMARY KEY,  
  dataInicio DATE NOT NULL,  
  dataFinal DATE NOT NULL,  
  temporada INT NOT NULL 
@@ -10,7 +10,7 @@ CREATE TABLE Campeonato
 
 CREATE TABLE Endereco 
 ( 
- id_endereco INT PRIMARY KEY,  
+ id_endereco SERIAL PRIMARY KEY,  
  rua VARCHAR(100),
  cidade VARCHAR(50) NOT NULL,   
  estado VARCHAR(2) NOT NULL,  
@@ -19,7 +19,7 @@ CREATE TABLE Endereco
 
 CREATE TABLE Piloto 
 ( 
- id_Piloto INT PRIMARY KEY,  
+ id_Piloto SERIAL PRIMARY KEY,  
  nomeCompleto VARCHAR(100) NOT NULL,  
  apelido VARCHAR(20) UNIQUE,  
  dataNascimento DATE, 
@@ -29,14 +29,14 @@ CREATE TABLE Piloto
 
 CREATE TABLE Categoria 
 ( 
- id_Categoria INT PRIMARY KEY,  
- nome VARCHAR(10) NOT NULL,
+ id_Categoria SERIAL PRIMARY KEY,  
+ nomeCategoria VARCHAR(10) NOT NULL,
  id_Piloto INT NOT NULL REFERENCES Piloto (id_Piloto)
 ); 
 
 CREATE TABLE Carro 
 ( 
- id_carro INT PRIMARY KEY, 
+ id_carro SERIAL PRIMARY KEY, 
  marca VARCHAR(50) NOT NULL, 
  modelo VARCHAR(50) NOT NULL,   
  motor VARCHAR(50), 
@@ -46,16 +46,16 @@ CREATE TABLE Carro
 
 CREATE TABLE Etapa 
 ( 
- id_Etapa INT PRIMARY KEY,  
+ id_Etapa SERIAL PRIMARY KEY,  
  nome_etapa VARCHAR(50) NOT NULL,  
- data DATE NOT NULL,  
+ dataEtapa DATE NOT NULL,  
  id_Endereco INT NOT NULL REFERENCES Endereco (id_Endereco),  
  id_Campeonato INT NOT NULL REFERENCES Campeonato (id_Campeonato)  
 ); 
 
 CREATE TABLE Patrocinio 
 ( 
- id_Patrocinio INT PRIMARY KEY,  
+ id_Patrocinio SERIAL PRIMARY KEY,  
  nome VARCHAR(100) NOT NULL,  
  cnpj VARCHAR(18) UNIQUE NOT NULL,  
  telefone VARCHAR(15),  
@@ -67,7 +67,7 @@ CREATE TABLE Patrocinio
 
 CREATE TABLE Ingresso 
 ( 
- id_ingresso INT PRIMARY KEY,  
+ id_ingresso SERIAL PRIMARY KEY,  
  nome VARCHAR(100) NOT NULL,  
  cpf VARCHAR(14) UNIQUE NOT NULL,  
  valor NUMERIC(7,2) NOT NULL,  
@@ -76,11 +76,9 @@ CREATE TABLE Ingresso
 
 CREATE TABLE Classificacao_etapa 
 ( 
- id_classificacao_etapa INT PRIMARY KEY,  
- classificacao INT NOT NULL,  
- posicao_etapa INT NOT NULL,  
+ id_classificacao_etapa SERIAL PRIMARY KEY,  
+ classificacao INT NOT NULL,    
  pontuacao INT NOT NULL,  
  id_Piloto INT NOT NULL REFERENCES Piloto (id_Piloto),  
  id_Etapa INT NOT NULL REFERENCES Etapa (id_Etapa) 
 ); 
-
